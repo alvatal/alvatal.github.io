fb9bc5efe271cec0f452fcebd6329a35c20b453f
Lauri Võsandi <lauri.vosandi@gmail.com>
2014-12-29 00:49:05 +0200

    Styling fixes

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,2 +1,8 @@
 $( document ).ready(function() {
+    $(".nav2 li").each(function(index, item) {
+        var link = $("a", item);
+        if (window.location.pathname == $(link).attr("href")) {
+            $(item).addClass("active");
+        }
+    });
 });
