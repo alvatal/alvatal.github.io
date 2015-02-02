1c98f6012f1a50274576b5d6379614fcc0150630
Lauri Võsandi <lauri.vosandi@gmail.com>
2015-02-02 19:57:27 +0100

    Fixed navbar highlighting

--- assets/js/app.js
+++
@@ -1,8 +0,0 @@
-$( document ).ready(function() {
-    $(".nav2 li").each(function(index, item) {
-        var link = $("a", item);
-        if (window.location.pathname == $(link).attr("href")) {
-            $(item).addClass("active");
-        }
-    });
-});
